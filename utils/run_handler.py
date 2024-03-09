b369b250c81eb8a234f5aec25584ae7ee6ab8cd8
Stefan Ghinescu <ghinescustefanalexandru95@gmail.com>
2024-03-09 10:38:31 +0200

    classes for configurations

--- utils/run_handler.py
+++ utils/run_handler.py
@@ -3,6 +3,49 @@ import periodictable
 import re
 import os
 import numpy as np
+from dhfs_handler import atomic_system
+
+class bound_config:
+    def __init__(self, params:dict) -> None:
+        self.max_r = params["max_r"]
+        if "radius_unit" in params:
+            a = params["radius_unit"]
+        else:
+            a = "bohr"
+        
+        self.radius_unit = a
+        self.n_radial_points = params["n_radial_points"]
+        
+        
+class scattering_config:
+    def __init__(self, params:dict) -> None:
+        self.max_r = params["max_r"]
+        if "radius_unit" in params:
+            a = params["radius_unit"]
+            if (a != "bohr") and (a != "fm"):
+                raise ValueError("radius_unit can be either absent, 'fm' or 'bohr'")
+        else:
+            a = "bohr"
+        
+        self.radius_unit = a
+        self.n_radial_points = params["n_radial_points"]
+        
+        self.min_ke = params["min_ke"]
+        self.max_ke = params["max_ke"]
+        
+        if "ke_unit" in params:
+            a = params["ke_unit"]
+            if (a != 'MeV' and (a != 'hartree')):
+                raise ValueError("ke_unit can be either absent, 'MeV' or 'hartree'")
+        else:
+            a = "MeV"
+        self.ke_unit = a
+        
+        self.n_ke_points = params["n_ke_points"]
+        self.ke_grid_type = params["ke_grid_type"]
+        if (self.ke_grid_type != 'lin') and (self.ke_grid_type != 'log'):
+            raise ValueError('ke_grid_type can be either "lin" or "log"')
+ 
         
 class run_input:
     def __init__(self, config_file_name:str) -> None:
@@ -12,10 +55,20 @@ class run_input:
         self.process_name = config["process"]
         self.output_dir = config["output_directory"]
         
-        self.initial_atom = {"name": config["initial_atom"]["name"],
-                             "weight": config["initial_atom"]["weight"],
-                             "electron_config": config["initial_atom"]["electron_config"]}
+        # atoms
+        self.initial_atom = atomic_system({"name": config["initial_atom"]["name"],
+                                           "weight": config["initial_atom"]["weight"],
+                                           "electron_config": config["initial_atom"]["electron_config"]})
         
-        self.final_atom = {"electron_config": config["final_atom"]["electron_config"]}
+        final_atom_dict = {"name": config["final_atom"]["name"],
+                           "weight": config["final_atom"]["weight"],
+                           "electron_config": config["final_atom"]["electron_config"]}
+        if (final_atom_dict["name"] == "auto"):
+            final_atom_dict["name"] = periodictable.elements[self.initial_atom.Z+2].symbol
+        self.final_atom = atomic_system(final_atom_dict)
+        
+        # technicals
+        self.bound_config = bound_config(config["bound_states"])
+        self.scattering_config = scattering_config(config["scattering_config"])
         
         
