b79b891bd4b920220be832a765115e96fc59c99f
Stefan Ghinescu <ghinescustefanalexandru95@gmail.com>
2024-03-10 12:12:47 +0200

    Towards completion

--- compute_spectra_psfs.py
+++ compute_spectra_psfs.py
@@ -7,6 +7,9 @@ from utils.dhfs_handler import dhfs_handler
 import numpy as np
 from utils import physics_constants
 
+from wrappers import radial_wrapper
+from wrappers.radial_wrapper import RADIALError
+
 import matplotlib.pyplot as plt
 
 def main(argv=None):
@@ -28,6 +31,7 @@ def main(argv=None):
                              input_config.bound_config.n_radial_points, 
                              input_config.bound_config.radius_unit)
     handler_initial.retrieve_dhfs_results()
+    rv_modified_initial = handler_initial.build_modified_potential()
     
     # bound states of final atom
     handler_final = dhfs_handler(input_config.final_atom, "final_atom")
@@ -37,18 +41,31 @@ def main(argv=None):
     handler_final.retrieve_dhfs_results()
     
     # build modified potentials
-    rv_modified_initial = handler_initial.build_modified_potential()
     rv_modified_final = handler_final.build_modified_potential()
-    
-    fig, ax = plt.subplots(figsize=(8,6))
-    ax.plot(handler_initial.rad_grid, rv_modified_initial)
-    ax.plot(handler_initial.rad_grid, handler_initial.rv_nuc+
-                                      handler_initial.rv_el+
-                                      handler_initial.rv_ex)
-    ax.set_xscale('log')
     
     # solve bound states in the initial atom
     
+    radial_wrapper.call_vint(handler_initial.rad_grid, rv_modified_initial)
+    radial_wrapper.call_setrgrid(handler_initial.rad_grid)
+    for i_s in range(len(handler_initial.dhfs_config.electron_config)):
+        print(f"Shell {i_s:d}")
+        n = handler_initial.dhfs_config.n_values[i_s]
+        l = handler_initial.dhfs_config.l_values[i_s]
+        j = 0.5*handler_initial.dhfs_config.jj_values[i_s]
+        k = int((l-j)*(2*j+1))
+        
+        print(f"n = {n:d}, k = {k:d}")
+        e_bound = 0.0
+        try:
+            e_bound = radial_wrapper.call_dbound(handler_initial.binding_energies[i_s],
+                                    n,
+                                    k,
+                                    eps=1E-14)
+        except RADIALError as e:
+            print("Problem in obtaining bound state ", e)
+        
+        
+        
     plt.show()
     
     
