93a7bd53d4327b1145ffa17076c84825655f0abb
Stefan Ghinescu <ghinescustefanalexandru95@gmail.com>
2024-03-12 04:53:51 +0200

    cleanup and print

--- compute_spectra_psfs.py
+++ compute_spectra_psfs.py
@@ -44,17 +44,15 @@ def main(argv=None):
     rv_modified_final = handler_final.build_modified_potential()
     
     # solve bound states in the initial atom
-    
+    input_config.bound_config.print()
     radial_wrapper.call_vint(handler_initial.rad_grid, rv_modified_initial)
     radial_wrapper.call_setrgrid(handler_initial.rad_grid)
     for i_s in range(len(handler_initial.dhfs_config.electron_config)):
-        print(f"Shell {i_s:d}")
         n = handler_initial.dhfs_config.n_values[i_s]
         l = handler_initial.dhfs_config.l_values[i_s]
         j = 0.5*handler_initial.dhfs_config.jj_values[i_s]
         k = int((l-j)*(2*j+1))
         
-        print(f"n = {n:d}, k = {k:d}")
         e_bound = 0.0
         try:
             e_bound = radial_wrapper.call_dbound(handler_initial.binding_energies[i_s],
